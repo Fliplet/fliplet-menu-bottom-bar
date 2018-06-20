fc08d41526c9f8be574ca6307f5a41d1314a3b1c
hcarneiro <hcarneiro@weboo.biz>
2018-06-20 12:36:58 +0100

    Tweaks and updates to background color

--- js/menu.js
+++ js/menu.js
@@ -10,7 +10,7 @@ function init() {
   var data = Fliplet.Widget.getData(menuInstanceId) || {};
 
   // Select active page
-  $('.fl-bottom-bar-menu-holder li[data-page-id="' + pageId + '"]').find('.fl-bottom-bar-icon-holder').addClass('active');
+  $('.fl-bottom-bar-menu-holder li[data-page-id="' + pageId + '"]').addClass('active');
 
   // Show more, when available
   $('li[data-show-more]').on('click', function() {
@@ -36,8 +36,8 @@ function init() {
   });
 
   $('.fl-bottom-bar-menu-holder li').not('[data-show-more]').on('click', function() {
-    $('.fl-bottom-bar-icon-holder').removeClass('active');
-    $(this).find('.fl-bottom-bar-icon-holder').addClass('active');
+    $('.fl-bottom-bar-menu-holder li').removeClass('active');
+    $(this).addClass('active');
   });
 
   $('[open-about-overlay]').on('click', function() {
