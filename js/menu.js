32e081df5aee5a3a77131b09705c19c3ece5814f
hcarneiro <hcarneiro@weboo.biz>
2018-11-02 18:36:23 +0000

    Revert changes to menu

--- js/menu.js
+++ js/menu.js
@@ -16,7 +16,26 @@ function init() {
   // Show more, when available
   $('li[data-show-more]').on('click', function() {
     var $parent = $(this).parents('.fl-bottom-bar-menu-holder');
-    $parent.toggleClass('expanded');
+    var totalMenuHeight = $parent.outerHeight(true);
+    var hiddenHeight = totalMenuHeight - 65; // 65 is what is visible from the start
+
+    if (!$parent.hasClass('expanded')) {
+      $parent.addClass('expanded').css({
+        'transform': 'translate3d(0px, -' + totalMenuHeight + 'px, 0)'
+      });
+    } else if (Modernizr.notch) {
+      $parent.removeClass('expanded').css({
+        'transform': 'translate3d(0px, calc(-65px - env(safe-area-inset-bottom)), 0)'
+      });
+    } else if ($('html').is('[data-has-notch]')) {
+      $parent.removeClass('expanded').css({
+        'transform': 'translate3d(0px, -99px, 0)'
+      });
+    } else {
+      $parent.removeClass('expanded').css({
+        'transform': 'translate3d(0px, -65px, 0)'
+      });
+    }
   });
 
   $('.fl-bottom-bar-menu-holder li').not('[data-show-more]').on('click', function() {
