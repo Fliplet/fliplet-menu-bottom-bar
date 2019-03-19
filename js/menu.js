f454a1116af668bb7b14e09e9dd0572c4ecae566
Tony Wu <twu@fliplet.com>
2019-03-19 11:39:38 +0000

    Ensures exit app links are added to the correct selector

--- js/menu.js
+++ js/menu.js
@@ -27,7 +27,7 @@ function init() {
     $li.on('click', function onExitClick() {
       Fliplet.Navigate.exitApp();
     });
-    $(this).find('ul').append($li);
+    $menuElement.find('ul').append($li);
 
     // Prevent default "Exit app" link from being added
     return Promise.reject('');
