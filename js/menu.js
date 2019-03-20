acc55bd75124afde0c0d89204fce025c407f04eb
Tony Wu <twu@fliplet.com>
2019-03-20 11:43:32 +0000

    Adds "more" link to menu before adding exit app link

--- js/menu.js
+++ js/menu.js
@@ -8,26 +8,55 @@ function init() {
 
   // Add exit app link
   Fliplet.Hooks.on('addExitAppMenuLink', function () {
-    if ($menuElement.find('li[data-fl-navigate*="exit-app"]').length) {
-      return;
-    }
+    $menuElement.find('.fl-bottom-bar-menu-holder').each(function () {
+      var $menuHolder = $(this);
+      var type = $menuHolder.hasClass('fl-bottom-bar-menu-holder-mobile') ? 'mobile' : 'tablet';
+      var maxIcons = {
+        mobile: 5,
+        tablet: 8
+      };
 
-    var $li = $([
-      '<li class="linked" data-fl-exit-app>',
-        '<div class="fl-bottom-bar-icon-holder">',
-          '<div class="fl-menu-icon">',
-            '<i class="fa fa-sign-out"></i>',
+      if ($menuHolder.find('li[data-fl-navigate*="exit-app"]').length) {
+        return;
+      }
+
+      if ($menuHolder.find('li').length === maxIcons[type]) {
+        var $shiftedMenuItem = $menuHolder.find('li').eq(maxIcons[type] - 1);
+        $shiftedMenuItem.css('display', 'none').before([
+          '<li data-show-more>',
+            '<div class="fl-bottom-bar-icon-holder">',
+              '<div class="fl-menu-icon">',
+                '<i class="fa fa-ellipsis-h"></i>',
+              '</div>',
+              '<div class="fl-menu-title">',
+                '<span>More</span>',
+              '</div>',
+            '</div>',
+          '</li>'
+        ].join(''));
+        $menuHolder.addClass('multiple');
+        setTimeout(function () {
+          $shiftedMenuItem.css('display', '');
+        }, 0);
+      }
+
+      var $li = $([
+        '<li class="linked" data-fl-exit-app>',
+          '<div class="fl-bottom-bar-icon-holder">',
+            '<div class="fl-menu-icon">',
+              '<i class="fa fa-sign-out"></i>',
+            '</div>',
+            '<div class="fl-menu-title">',
+              '<span>Exit app</span>',
+            '</div>',
           '</div>',
-          '<div class="fl-menu-title">',
-            '<span>Exit app</span>',
-          '</div>',
-        '</div>',
-      '</li>'
-    ].join(''));
-    $li.on('click', function onExitClick() {
-      Fliplet.Navigate.exitApp();
+        '</li>'
+      ].join(''));
+      $li.on('click', function onExitClick() {
+        Fliplet.Navigate.exitApp();
+      });
+      $menuHolder.find('ul').append($li);
     });
-    $menuElement.find('ul').append($li);
 
     // Prevent default "Exit app" link from being added
     return Promise.reject();
@@ -37,17 +66,18 @@ function init() {
   $('.fl-bottom-bar-menu-holder li[data-page-id="' + pageId + '"]').addClass('active');
 
   // Show more, when available
-  $('li[data-show-more]').on('click', function() {
+  $menuElement.on('click', 'li[data-show-more]', function() {
     var $parent = $(this).parents('.fl-bottom-bar-menu-holder');
     $parent.toggleClass('expanded');
   });
 
-  $('.fl-bottom-bar-menu-holder li').not('[data-show-more]').on('click', function() {
-    $('.fl-bottom-bar-menu-holder li').removeClass('active');
+  $menuElement.on('click', '.fl-bottom-bar-menu-holder li:not([data-show-more])', function() {
+    $('.fl-bottom-bar-menu-holder li.active').removeClass('active');
     $(this).addClass('active');
   });
 
-  $('[open-about-overlay]').on('click', function() {
+  $menuElement.on('click', '[open-about-overlay]', function(e) {
+    e.preventDefault();
     Fliplet.Navigate.to({
       action: 'about-overlay'
     });
