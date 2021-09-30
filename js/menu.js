49b6d618a4a11370b462635b9494c9cb81daa486
Tony Wu <tonytlwu@users.noreply.github.com>
2021-09-30 19:41:12 +0100

    Select menu item based on index

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
 var $menuElement = $('[data-name="Bottom icon bar"]');
 var menuInstanceId = $menuElement.data('id');
 
+function highlightItemByIndex(index) {
+  $('.fl-bottom-bar-menu-holder')
+    .each(function() {
+      $(this).find('li')
+        .not('[data-show-more]') // Ignore "More" menu items
+        .eq(index).addClass('active');
+    });
+}
+
 function init() {
   var data = Fliplet.Widget.getData(menuInstanceId) || {};
   $('body').addClass('fl-menu-bottom-bar');
@@ -66,12 +75,7 @@ function init() {
 
   // Select active page based on query
   if (!isNaN(activeMenuItem)) {
-    $('.fl-bottom-bar-menu-holder')
-      .each(function() {
-        $(this).find('li')
-          .not('[data-show-more]') // Ignore "More" menu items
-          .eq(activeMenuItem).addClass('active');
-      })
+    highlightItemByIndex(activeMenuItem);
   } else {
     // Select active page based on current page ID
     $('.fl-bottom-bar-menu-holder li[data-page-id="' + Fliplet.Env.get('pageId') + '"]').addClass('active');
@@ -103,6 +107,16 @@ function init() {
     }
 
     $('.fl-bottom-bar-menu-holder li.active').removeClass('active');
+
+    var navigate = $(this).data('fl-navigate');
+    var index = Fliplet.Navigate.parseQuery(navigate.query).activeMenuItem;
+
+    if (typeof index !== 'undefined') {
+      highlightItemByIndex(index);
+
+      return;
+    }
+
     $(this).addClass('active');
   });
 
